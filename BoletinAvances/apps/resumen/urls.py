76c84b31b1a210ae9c8058ca515f130c923d4f03
JMEspiz <josemespiz@gmail.com>
2016-03-12 14:12:53 -0430

    ADD - URLs para Resumen de noticias

--- BoletinAvances/apps/resumen/urls.py
+++ BoletinAvances/apps/resumen/urls.py
@@ -5,6 +5,13 @@ from BoletinAvances.apps.resumen.views import *
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('BoletinAvances.apps.avances.views',
+
+    #NoticiaResumen
+    url(r'^NoticiaResumenver/?$', login_required(NoticiaResumenListView.as_view()), name='ver_noticia_resumen'),
+    url(r'^NoticiaResumendetalle-(?P<slug>[-\w]+)/$', login_required(NoticiaResumenDetailView.as_view()), name = 'noticia_resumen_detalle'),
+    url(r'^NoticiaResumenenviar/?$', login_required(NoticiaResumenAddView.as_view()), name = 'enviar_noticia_resumen'),
+    url(r'^NoticiaResumendel-(?P<slug>[-\w]+)/$', login_required(NoticiaResumenDeleteView.as_view()), name = 'noticia_resumen_delete'),
+
     #Resumen
     url(r'^Resumenver/?$', login_required(ResumenListView.as_view()), name='ver_resumen'),
     url(r'^Resumendetalle-(?P<slug>[-\w]+)/$', login_required(ResumenDetailView.as_view()), name = 'resumen_detalle'),
