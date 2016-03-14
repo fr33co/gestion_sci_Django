6ec3126b7e03c31190927a92c395dded35fa2e3f
JMEspiz <josemespiz@gmail.com>
2016-03-14 09:33:03 -0430

    CHANGE - View y Models de Resumen

--- BoletinAvances/apps/resumen/models.py
+++ BoletinAvances/apps/resumen/models.py
@@ -12,10 +12,16 @@ TAG = (
     ('SUC', 'Sucesos'),
 )
 
+TIPO_ENVIO = (
+    ('Automatico', 'Automatico'),
+    ('Programado', 'Programado'),
+)
+
 class Resumen(models.Model):
     ''' Clase para Resumen '''
     titulo_mensaje = models.CharField(max_length=150, verbose_name='Titulo', default='')
     cuerpo_mensaje = models.TextField(verbose_name='Cuerpo el mensaje', default='')
+    tipo_envio = models.CharField(max_length=20, choices=TIPO_ENVIO, default='', verbose_name='Tipo de envio')
     fecha = models.DateField(default=datetime.now, blank=True, null=True)
     hora = models.TimeField(auto_now_add=True, blank=True, null=True)
     status = models.CharField(max_length=110, verbose_name='Status', blank=True, null=True)
@@ -38,6 +44,9 @@ class NoticiaResumen(models.Model):
     cuerpo_noticia_resumen = models.TextField(verbose_name='Cuerpo del resumen', default='')
     tag_noticia_resumen = models.CharField(max_length=30, verbose_name='Etiqueta', choices=TAG, default='')
     url_noticia_resumen = models.TextField(verbose_name='URLs')
+    status = models.CharField(max_length=110, verbose_name='Status', blank=True, null=True)
+    enviadopor_noticia_resumen = models.CharField(max_length=50, verbose_name='Enviado por:', default='')
+    fecha_noticia_resumen = models.DateField(default=datetime.now, blank=True, null=True)
 
     class Meta:
         verbose_name_plural = "Agregar Resumen de Noticia"
