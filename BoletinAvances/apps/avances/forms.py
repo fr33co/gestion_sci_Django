c11cfa2a734bd525abf85f76e6a56c0344064d45
JMEspiz <josemespiz@gmail.com>
2016-04-06 08:29:17 -0430

    añade formset

--- BoletinAvances/apps/avances/forms.py
+++ BoletinAvances/apps/avances/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms.models import inlineformset_factory
-from BoletinAvances.apps.avances.models import Avances, Diarios, Noticias
+from BoletinAvances.apps.avances.models import Avances, Diarios, Noticias, EnlaceDiarios
 
 
 class DiariosForm(forms.ModelForm):
@@ -19,9 +19,15 @@ class AvancesForm(forms.ModelForm):
 class NoticiasForm(forms.ModelForm):
     class Meta:
         model = Noticias
-        exclude = ('avances', 'enviadopor')
+        exclude = ('avances', 'enviadopor',)
         fields = '__all__'
 
+class EnlaceDiariosForm(forms.ModelForm):
+    class Meta:
+        model = EnlaceDiarios
+        exclude = ('noticias',)
+        field = '__all__'
 
 NoticiasFormSet = inlineformset_factory(Avances, Noticias, fields = '__all__', can_delete=True, extra=0)
+EnlaceDiariosFormSet = inlineformset_factory(Noticias, EnlaceDiarios, fields = '__all__', can_delete=True, extra=0)
 item_forms = NoticiasFormSet()
